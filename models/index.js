33cb538519766a4ded111786497dc8a5fa8289bf
ravitupkar <ravitupkar01@gmail.com>
2019-12-13 17:19:41 +0530

    first commit

---
+++ models/index.js
@@ -0,0 +1,5 @@
+
+
+module.exports.Users = require('./Users');
+module.exports.Contact = require('./Contact');
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var indexController = require('../controllers/indexController');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/create-contact', indexController.createContact);
 
 module.exports = router;
